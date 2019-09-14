d4cdb733ef84cd4e6ac60b8280252918547d934c
Naston <nastik124@gmail.com>
2019-09-14 20:09:52 +0300

    Fully complete samrab1-2

--- samrab1-2/main.c
+++ samrab1-2/main.c
@@ -10,6 +10,7 @@
  ****************************************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
 
 /****************************************************************************
  *                                                                          *
@@ -21,7 +22,64 @@
  *           00/00/00  Created                                              *
  *                                                                          *
  ****************************************************************************/
+int chil (int c)
+{
+    int count=0;
+    while (c != 0)
+    {
+        c = c/10;
+        count++;
+    }
+    return count;
+}
+void a_prog(int f, int kolvo, int raznost)
+{
+    printf("---------");
+    for (int i = 0; i < kolvo; i++)
+    {
+      printf("------");
+    }
 
+
+    printf("\nIndex   | ");
+    for (int i = 0; i < kolvo; i++)
+    {
+        printf("%d ", i+1);
+        for(int j = 0; j<4-chil(i+1);j++)
+            {
+                printf(" ");
+            }
+            printf("|");
+    }
+    printf("\n");
+    printf("---------");
+    for (int i = 0; i < kolvo; i++)
+    {
+      printf("------");
+    }
+    printf("\nNumbers | ");
+    for (int i = 0; i < kolvo; i++)
+    {
+            printf("%d ", f + raznost*i);
+            for(int j = 0; j<4-chil(f+raznost*i);j++)
+            {
+                printf(" ");
+            }
+            printf("|");
+    }
+    printf("\n");
+    printf("---------");
+    for (int i = 0; i < kolvo; i++)
+    {
+      printf("------");
+    }
+
+}
+void sum (int first, int kol, int razn)
+{
+    printf("\nsumma = %d\n", (((2*first + (kol-1)*razn)*kol)/2));
+    printf("summa the last + 1 = %d\n", 3*first+3*kol*razn);
+}
 int main()
 {
 	int a,n,d,x,s;
@@ -31,10 +89,9 @@ int main()
 	scanf("%d",&n);
     printf("Input  d:\n");
 	scanf("%d",&d);
-		s=a*n+((n-1)*d*n)/2;
-        x=3*a+3*n*d-3*d;
-        printf("Sum s=%d\n",s);
-        printf("Sum2 x=%d\n",x);
+
+	a_prog(a,n,d);
+	sum(a, n, d);
 
 
     return 0;
